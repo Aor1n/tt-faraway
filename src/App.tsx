31423d7bb55ab8e6aa99846dc94235a9f9e51294
vladislav <unbeliever1806@gmail.com>
2023-07-27 20:34:45 +0400

    build: react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Users } from '@UI/Users';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { COLORS } from '@/colors';
 import { ModalProvider } from '@/providers/ModalProvider';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const theme = createTheme({
   palette: {
@@ -15,6 +18,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <ModalProvider>
+        <ToastContainer />
         <Users />
       </ModalProvider>
     </ThemeProvider>
