193aed801a463a05e18164f9a317271825e95bc4
vladislav <unbeliever1806@gmail.com>
2023-07-30 22:14:24 +0400

    build: @testing-library & jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
@@ -12,4 +15,8 @@ export default defineConfig({
     ],
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
