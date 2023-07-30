7af156af6761acce4480ac14c1b4e62a6588feeb
vladislav <unbeliever1806@gmail.com>
2023-07-30 22:49:13 +0400

    test: Search prop update

--- src/__tests__/Search.test.tsx
+++ src/__tests__/Search.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Search } from '@UI/Search';
 
 describe('Search test', () => {
-  render(<Search value={''} setSearch={vi.fn} setInitialPaginationPage={vi.fn} label={`Search by character's name`} />);
+  render(<Search value={''} setSearch={vi.fn} label={`Search by character's name`} />);
 
   test('Search has label', () => {
     expect(screen.getByLabelText("Search by character's name")).toBeDefined();
