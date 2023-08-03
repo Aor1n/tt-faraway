57049310276477e32ffcc515a98a38274c6fdf0e
vladislav <unbeliever1806@gmail.com>
2023-08-03 20:18:33 +0400

    test: Search

--- src/__tests__/Search.test.tsx
+++ src/__tests__/Search.test.tsx
@@ -2,10 +2,47 @@ import { describe, test, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Search } from '@UI/Search';
 
-describe('Search test', () => {
-  render(<Search value={''} setSearch={vi.fn} label={`Search by character's name`} />);
+import matchers from '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+expect.extend(matchers);
 
-  test('Search has label', () => {
-    expect(screen.getByLabelText("Search by character's name")).toBeDefined();
+function setup() {
+  const setSearchMock = vi.fn();
+
+  return {
+    user: userEvent.setup(),
+    ...render(<Search setSearch={setSearchMock} />),
+  };
+}
+
+describe('Search test', () => {
+  test('Search changes its value if user types', async () => {
+    const { user } = setup();
+    const input = screen.getByRole('textbox');
+    const value = 'I am text';
+
+    expect(input).toHaveValue('');
+
+    await user.type(input, value);
+
+    expect(input).toHaveValue(value);
+  });
+
+  test('Search drops its value on Clear button click', async () => {
+    const { user } = setup();
+    const input = screen.getByRole('textbox');
+    const button = screen.getByRole('button');
+    const initialValue = '';
+    const value = 'I am text';
+
+    expect(input).toHaveValue(initialValue);
+
+    await user.type(input, value);
+
+    expect(input).toHaveValue(value);
+
+    await user.click(button);
+
+    // await waitFor(() => expect(input).toHaveValue(initialValue));
   });
 });
